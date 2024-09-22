10ecb8c6b628465215f559a758f317c7c72aa396
IMP1 <huw_taylor@hotmail.co.uk>
2024-09-22 19:35:18 +0100

    Add functions for audio player

---
+++ player.js
@@ -0,0 +1,55 @@
+function play(songUrl) {
+    
+}
+
+function addToQueue(songUrl) {
+
+}
+
+function pause() {
+
+}
+
+function resume() {
+
+}
+
+function skip() {
+
+}
+
+function rewind() {
+
+}
+
+function toggleLoop() {
+
+}
+
+function toggleShuffle() {
+
+}
+
+function showQueue() {
+
+}
+
+function showLyrics() {
+    
+}
+
+function showSongInfo() {
+    
+}
+
+function download() {
+    
+}
+
+
+
+function setup() {
+    console.log("Setting up");
+}
+
+window.addEventListener("load", setup);
