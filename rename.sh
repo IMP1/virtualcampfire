25d3e328f654610a16b6c8cba1a6ef85de005681
Tim Pizey <PIZEYT@science.regn.net>
2024-11-05 20:36:53 +0000

    Rename

---
+++ rename.sh
@@ -0,0 +1,12 @@
+# Rename an mp3 and all references to it
+# Usage: rename.sh <old> <new>
+# Example: rename.sh Oh_Lord_Won\'t_You_Buy_me_A_Mercedes_Benz_\(02\).mp3 Oh_Lord_Won\'t_You_Buy_me_A_Mercedes_Benz_\(03\).mp3
+
+old=$1
+new=$2
+
+mv mp3/$old mp3/$new
+sed -i "s/$old/$new/g" *.html
+sed -i "s/$old/$new/g" *.txt
+echo "Do lyrics manually"
+
